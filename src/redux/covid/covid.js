82e554476443107f5ff1bbcabd8ee41507f323fa
Anand8317 <43726580+Anand8317@users.noreply.github.com>
2023-04-17 16:37:07 +0530

    setup store

---
+++ src/redux/covid/covid.js
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const FETCH_COVID_ONCE = 'covid-10-india/covid/FETCH_COVID_ONCE';
+
+const url = 'https://data.covid19india.org/v4/min/data.min.json';
+
+const fetchCovidAction = createAsyncThunk(
+  FETCH_COVID_ONCE,
+  async (post, { dispatch }) => {
+    const res = await fetch(url);
+    const resJson = await res.json();
+    // const covid = await resJson.map((item) => ({
+    //   mission_id: item.mission_id,
+    //   mission_name: item.mission_name,
+    //   description: item.description,
+    // }));
+    dispatch({ type: FETCH_COVID_ONCE, covid });
+  },
+);
+
+export function covidReducer(state = [], action) {
+  switch (action.type) {
+    case FETCH_COVID_ONCE:
+      return action.covid;
+    default: return state;
+  }
+}
+
+export { fetchCovidAction };
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore, applyMiddleware } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import { covidReducer } from './covid/covid';
 
 const store = configureStore(
   {
